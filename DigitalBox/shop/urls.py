af1cd647f9baf2b658649892fb6b9cbb4e5c29e5
Aleksandr585988 <koltsovoleksandr10081989@gmail.com>
2025-06-28 19:47:09 +0000

    init

--- DigitalBox/shop/urls.py
+++ DigitalBox/shop/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.content_list, name='content'),
-    path('laptops/', views.laptops_and_computers, name='laptops_and_computers'),
+    path('laptopsAndComputer/', views.laptops_and_computers, name='laptops_and_computers'),
+    path('category/<int:category_id>/', views.products_by_category, name='products_by_category'),
+    path('productslist/', views.product_list, name='product_list'),
+    path('productslaptop/', views.product_laptop, name='product_laptop'),
     ]
+    
--- DigitalBox/shop/views.py
+++ DigitalBox/shop/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
-from .models import Product, Content
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Content, Category
+
+def content_list(request):
+    categories = Category.objects.all()
+    content = Content.objects.all()
+    return render(request, 'shop/content.html', {'content': content, 'categories': categories})
+
+def laptops_and_computers(request):
+    categories = Category.objects.all()
+    return render(request, 'shop/laptops_and_computers.html', {
+        'categories': categories
+    })
 
 def product_list(request):
     product = Product.objects.filter(available=True)
     return render(request, 'shop/product_list.html', {'products': product})
 
-def content_list(request):
-    content = Content.objects.filter()
-    return render(request, 'shop/content.html', {'content': content})
+def product_laptop(request):
+    product = Product.objects.filter(available=True)
+    return render(request, 'shop/product_laptop.html', {'products': product})
 
-def laptops_and_computers(request):
-    return render(request, 'shop/laptops_and_computers.html')
+def products_by_category(request, category_id):
+    category = get_object_or_404(Category, id=category_id)
+    products = Product.objects.filter(category=category, available=True)
+    return render(request, 'shop/products_by_category.html', {
+        'category': category,
+        'products': products
+    })
