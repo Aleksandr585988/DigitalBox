6f4b5fe5f12664e111e181646f6540437fdff799
Aleksandr585988 <koltsovoleksandr10081989@gmail.com>
2025-07-06 11:49:52 +0000

    init

--- DigitalBox/shop/urls.py
+++ DigitalBox/shop/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.content_list, name='content'),
     path('laptopsAndComputer/', views.laptops_and_computers, name='laptops_and_computers'),
     path('category/<int:category_id>/', views.products_by_category, name='products_by_category'),
-    path('productslist/', views.product_list, name='product_list'),
-    path('productslaptop/', views.product_laptop, name='product_laptop'),
+    path('productslist/', views.products_list, name='product_list'),
+    path('productslaptop/', views.products_laptop, name='product_laptop'),
     ]
     
