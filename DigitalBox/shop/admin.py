d625c49c1daa64f604239cc17194b2f416ee3f7c
Aleksandr585988 <koltsovoleksandr10081989@gmail.com>
2025-07-06 11:58:21 +0000

    replaced the admin registration method with admin register

--- DigitalBox/shop/admin.py
+++ DigitalBox/shop/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from.models import Category, Product, Content
+from .models import Content, Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Content)
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "category", "price", "available")
+    list_filter = ("available", "category")
+    search_fields = ("name", "description")
